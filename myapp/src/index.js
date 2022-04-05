a6deccf080b2bae55c02f0ade2de81ab1dbdf5c0
Levoshchenko <chrislewoshenko@gmail.com>
2022-04-05 18:12:50 +0900

    les3

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createTheme, ThemeProvider} from '@mui/material';
+import {purple} from '@mui/material/colors';
+
+
+const currentName = "Kristina";
+
+const theme = createTheme({
+  palette:{
+    mode: 'light',
+  },
+  status: {
+    danger: purple[200],
+  },
+
+});
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <App name={currentName} topPosition="100px"/>
+</ThemeProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
